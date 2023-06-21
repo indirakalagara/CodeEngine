cfeac1efc28826c36cf2f5884dc9bfe3534708a7
Indira <indira@192.168.1.7>
2023-06-21 15:12:31 +0530

    updated with more recores
    Signed-off-by: Indira <indira@192.168.1.7>

--- helloworld-samples/app-nodejs/index.js
+++ helloworld-samples/app-nodejs/index.js
@@ -14,14 +14,53 @@ app.use("/", router)
 router.get("/", (req,res) => {
 	//res.send("hello world!")
     const sevOneData={
-        "Indicator": "CarbonOutput",
-        "Timestamp": "1682958004000",
-        "dataCenter": "MSPWG-LON-EDGE-1",
-        "deviceId": "56503",
-        "emissionValue": "3.78625",
-        "location": "UK",
-        "value": "2.42708333333333"
-    }
+        "sevOneData":[ 
+          {
+              "Indicator": "CarbonOutput",
+              "Timestamp": "1682958004000",
+              "dataCenter": "MSPWG-BCN-CORE-1",
+              "deviceId": "56509",
+              "emissionValue": "3.30083333333333",
+              "location": "Spain"
+      },
+      {
+              "Indicator": "CarbonOutput",
+              "Timestamp": "1682958004000",
+              "dataCenter": "MSPWG-BCN-EDGE-1",
+              "deviceId": "56507",
+              "emissionValue": "3.32025",
+              "location": "Spain"
+              
+      },
+          {
+              "Indicator": "CarbonOutput",
+              "Timestamp": "1682958004000",
+              "dataCenter": "MSPWG-DNK-EDGE-1",
+              "deviceId": "56511",
+              "emissionValue": "2.71833333333333",
+              "location": "Denmark"
+      },
+      
+      {
+              "Indicator": "CarbonOutput",
+              "Timestamp": "1682958004000",
+              "dataCenter": "MSPWG-LON-CORE-1",
+              "deviceId": "56505",
+              "emissionValue": "3.78625",
+              "location": "London"
+      },
+      {
+              "Indicator": "CarbonOutput",
+              "Timestamp": "1682958004000",
+              "dataCenter": "MSPWG-LON-EDGE-1",
+              "deviceId": "56503",
+              "emissionValue": "3.78625",
+              "location": "London",
+              "value": "3.78625"
+      }
+        ]
+      }
+      
     res.json(sevOneData);
     res.end();
 })
